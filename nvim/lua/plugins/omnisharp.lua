a67a277bfb4ca19f44c022e6833167be0dc3aa3a
Andre Cato <andrecato@gmail.com>
2023-01-09 15:04:43 -0500

    Cleanup keybindings for Omnisharp

--- nvim/lua/plugins/omnisharp.lua
+++ nvim/lua/plugins/omnisharp.lua
@@ -4,17 +4,6 @@
 --    return
 --end
 
-local keymap = vim.keymap -- for conciseness
 
 vim.g["OmniSharp_server_path"] = os.getenv("HOME") .. "/.omnisharp/OmniSharp.exe"
 
--- OmniSharp bindings
-keymap.set("n", "<leader>rt", ":OmniSharpRunTests<CR>")
---keymap.set("n", "<leader>rt", ":TestNearest<CR>")
-keymap.set("n", "<leader>rf", ":OmniSharpRunTestFixture<CR>")
-keymap.set("n", "<leader>ra", ":OmniSharpRunAllTest<CR>")
-keymap.set("n", "<leader>rl", ":OmniSharpRunLastTest<CR>")
-keymap.set("n", "<leader>t", ":OmniSharpHighlightTypes<CR>")
-keymap.set("n", "<leader>b",  ":OmniSharpBuildAsync<CR>")
-keymap.set("n", "<leader><Space>", ":OmniSharpGetCodeActions<CR>")
-keymap.set("n", "<leader>nm", ":OmniSharpRename<CR>")
