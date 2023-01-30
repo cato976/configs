a38a0182db1a258e2fce0f7a973fc835d792f851
Andre Cato <andrecato@gmail.com>
2023-01-30 10:11:18 -0500

    Remove duplicate key binding
    *
    
    This change addresses the need by:
    
    *

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -8,7 +8,6 @@ keymap.set("n", "<leader>l", "<C-w>l")
 keymap.set("n", "<leader>h", "<C-w>h")
 keymap.set("n", "<leader>k", "<C-w>k")
 keymap.set("n", "<leader>j", "<C-w>j")
-keymap.set("n", "<leader>q", "<C-w>q") -- nnoremap <leader>q :call <SID>QuitWindow()<CR> 
 keymap.set("n", "<C-f>", ":Rg ") -- fuzy find
 keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move highligthed text down
 keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move highligthed text up
@@ -16,8 +15,6 @@ keymap.set("n", "<leader>bb", ":buffers<CR>")
 
 -- nvim-tree
 vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-
---nnoremap <leader>b :Neomake<cr>
 
 -- fzf
 keymap.set("n", "<leader>o", ":Files<CR>")
